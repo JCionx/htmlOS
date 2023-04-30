d3c4f007eddb27cbf18fea0600919d932ee2bee8
JCionx <jcionx0@gmail.com>
2023-04-30 16:45:06 +0100

    3

--- scripts/controls.js
+++ scripts/controls.js
@@ -8,3 +8,8 @@ function maximize(element) {
     element.parentElement.parentElement.parentElement.style.top = '0';
     element.parentElement.parentElement.parentElement.style.left = '0';
 }
+
+function minimize(element) {
+    element.parentElement.parentElement.parentElement.style.width = element.parentElement.parentElement.parentElement.style.minWidth;
+    element.parentElement.parentElement.parentElement.style.height = element.parentElement.parentElement.parentElement.style.minHeight;
+}
